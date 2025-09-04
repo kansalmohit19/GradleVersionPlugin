7a51c99ed4ba27964a6db31149438de565e1ed8e
mohitkansal <kansalmohit19@gmail.com>
2025-09-04 14:57:11 +0530

    Changed added

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
     id("com.gradle.plugin-publish") version "1.2.1"
-    id("signing")
 }
 
 repositories {
@@ -14,16 +13,16 @@ group = "io.github.kansalmohit19"
 version = "1.0.1"
 
 gradlePlugin {
-    website = "https://github.com/kansalmohit19/GradleVersionPlugin"
-    vcsUrl = "https://github.com/kansalmohit19/GradleVersionPlugin.git"
-    //tags = listOf("android", "gradle", "versioning", "git")
+    website.set("https://github.com/kansalmohit19/GradleVersionPlugin")
+    vcsUrl.set("https://github.com/kansalmohit19/GradleVersionPlugin")
 
     plugins {
         create("gradleVersionPlugin") {
-            id = "com.mohitkansal.gradle-version"   // plugin id used in builds
+            id = "com.mohitkansal.gradle-version"   // plugin id users apply
             implementationClass = "com.mohitkansal.GradleVersionPlugin"
             displayName = "Gradle Version Plugin"
             description = "Automatically generates versionCode and versionName from Git."
+            tags.set(listOf("android", "gradle", "versioning", "git"))
         }
     }
 }
